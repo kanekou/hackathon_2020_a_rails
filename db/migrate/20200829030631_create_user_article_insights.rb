aa4eb97e876a45c3d72ad86909b83577063cb4e8
kanekou <e165726@ie.u-ryukyu.ac.jp>
2020-08-29 12:11:46 +0900

    user_article_insight

---
+++ db/migrate/20200829030631_create_user_article_insights.rb
@@ -0,0 +1,10 @@
+class CreateUserArticleInsights < ActiveRecord::Migration[6.0]
+  def change
+    create_table :user_article_insights do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :article, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_29_025827) do
+ActiveRecord::Schema.define(version: 2020_08_29_030631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,15 @@ ActiveRecord::Schema.define(version: 2020_08_29_025827) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "user_article_insights", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_user_article_insights_on_article_id"
+    t.index ["user_id"], name: "index_user_article_insights_on_user_id"
+  end
+
   create_table "user_topic_insigthts", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "topic_id", null: false
@@ -63,6 +72,8 @@ ActiveRecord::Schema.define(version: 2020_08_29_025827) do
 
   add_foreign_key "adresses", "users"
   add_foreign_key "articles", "topics"
+  add_foreign_key "user_article_insights", "articles"
+  add_foreign_key "user_article_insights", "users"
   add_foreign_key "user_topic_insigthts", "topics"
   add_foreign_key "user_topic_insigthts", "users"
 end
