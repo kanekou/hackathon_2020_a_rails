7f9bcf8f3fff7efcbba863c605ea547d151fe1ec
d-sawada <d_sawada@ga-tech.co.jp>
2020-08-29 17:46:46 +0900

    routesに追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :users, only: [:show, :update]
+    resource :authenticate, only: [] do
+      collection do
+        post :sign_up
+        post :login
+      end
+    end
+  end
 end
