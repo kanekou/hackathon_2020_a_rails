74dc69e54c8fc4706fe6b66d42179d3919a1bc36
tj16kimura <kawalab.19.kimura@gmail.com>
2020-08-29 05:13:48 +0000

    /get_news エンドポイントを作成．動作確認済み

--- flask_app/adapter.wsgi
+++ flask_app/adapter.wsgi
@@ -1,4 +1,4 @@
 import sys
-sys.path.insert(0, '/var/www/sample')
+sys.path.insert(0, '/var/www/hackathon_2020_a_rails/flask_app')
 
 from app import app as application
--- flask_app/app.py
+++ flask_app/app.py
@@ -1,14 +1,37 @@
 from flask import Flask, jsonify
+from newsapi import NewsApiClient
+from datetime import datetime as dt
+from datetime import timedelta
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+newsapi = NewsApiClient(api_key='d9cc224113f649869ee2ee4e25198595')
 
 @app.route("/")
 def hello():
-    return "Hello, Flask!"
+    return "Hello, Flask! in Repository"
 
 @app.route('/test')
 def test():
-    return jsonify({'message': 'test is ok'})
+    return jsonify({'message': 'test is ok.'})
+
+@app.route('/get_news')
+def get_news():
+    headlines = newsapi.get_top_headlines(country='jp')
+    ret = {}
+    articles = []
+    for a in headlines['articles']:
+        item = {}
+        item['title'] = a['title']
+        item['summary'] = a['description']
+        item['url'] = a['url']
+        item['image'] = a['urlToImage']
+        item['written_at'] = (dt.strptime(a['publishedAt'], '%Y-%m-%dT%H:%M:%SZ') + timedelta(hours=9)).strftime('%Y-%m-%d %H:%M:%S')
+        articles.append(item)
+
+    ret['articles'] = articles
+    return jsonify(ret)
 
 if __name__ == "__main__":
     app.run()
