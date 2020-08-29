9255717482e4319bc0a4fc3ce8e5f84bdda6fde2
d-sawada <d_sawada@ga-tech.co.jp>
2020-08-29 18:39:54 +0900

    配信日時一覧のcontroller

---
+++ app/controllers/api/user_recommended_news_histories_controller.rb
@@ -0,0 +1,12 @@
+class Api::UserRecommendedNewsHistoriesController < ApiController
+  def index
+    response = {
+      user_recommended_news_histories: [
+        current_user.user_recommended_news_histories.map do |history|
+          { created_at: histroy.created_at.strftime("%Y/%m/%d %H:%M") }
+        end
+      ]
+    }
+    render json: response
+  end
+end
