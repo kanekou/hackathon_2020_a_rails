311f6a84d41be085cf359f44c87fcb544a4b82cd
kanekou <e165726@ie.u-ryukyu.ac.jp>
2020-08-29 13:30:57 +0900

    Fix association around user_topics.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,6 @@
 class Topic < ApplicationRecord
-  has_many :topics
+  has_many :user_topic_insights
+  has_many :users, through: :user_topic_insights
   validates :name, presence: true, uniqueness: true
   validates :default_delivery_time, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :addresses
   has_many :user_topic_insights
+  has_many :topics, through: :user_topic_insights
   has_many :user_recommended_news_histories
   has_many :user_articles_insights
   validates :nickname, presence: true
