cb44a2ca3e17984ad6735c84f2cd659665a82a30
d-sawada <d_sawada@ga-tech.co.jp>
2020-08-30 11:05:11 +0900

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,21 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 [
-  [Time.parse("7:00"), :morning],
-  [Time.parse("12:00"), :daytime],
-  [Time.parse("19:00"), :night]
-].each do |recommend_time, period|
-  MRecommendedNewsTime.create!(recommend_time: recommend_time, period: period)
+  { id: 1, recommend_time: Time.parse("7:00"), period: :morning },
+  { id: 2, recommend_time: Time.parse("12:00"), period: :daytime },
+  { id: 3, recommend_time: Time.parse("19:00"), period: :night },
+].each do |params|
+  MRecommendedNewsTime.find_or_initialize_by(id: params[:id]).update!(params)
 end
 
 [
-  { name: "コロナ", code: "COVID-19", default_delivery_time: Time.parse("08:00") },
-  { name: "熱中症", code: "heartstroke", default_delivery_time: Time.parse("08:00") },
-  { name: "健康/運動", code: "exercise", default_delivery_time: Time.parse("08:00") },
-  { name: "ライフハック", code: "lifehack", default_delivery_time: Time.parse("08:00") },
-  { name: "メンタルヘルス", code: "mental", default_delivery_time: Time.parse("08:00") },
-  { name: "食事", code: "meal", default_delivery_time: Time.parse("08:00") },
-  { name: "睡眠", code: "sleep", default_delivery_time: Time.parse("08:00") },
+  { id: 1, name: "コロナ", code: "COVID-19", default_delivery_time: Time.parse("08:00") },
+  { id: 2, name: "熱中症", code: "heartstroke", default_delivery_time: Time.parse("08:00") },
+  { id: 3, name: "健康/運動", code: "exercise", default_delivery_time: Time.parse("08:00") },
+  { id: 4, name: "ライフハック", code: "lifehack", default_delivery_time: Time.parse("08:00") },
+  { id: 5, name: "メンタルヘルス", code: "mental", default_delivery_time: Time.parse("08:00") },
+  { id: 6, name: "食事", code: "meal", default_delivery_time: Time.parse("08:00") },
+  { id: 7, name: "睡眠", code: "sleep", default_delivery_time: Time.parse("08:00") },
 ].each do |params|
-  Topic.create(params)
+  Topic.find_or_initialize_by(id: params[:id]).update!(params)
 end
