f84a1c86df648caf8fef3aaa9f7d3b6bb7b26478
kanekou <e165726@ie.u-ryukyu.ac.jp>
2020-08-30 10:35:45 +0900

    Add m_recommended_news_time seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+[
+    [Time.parse("7:00"), :morning],
+    [Time.parse("12:00"), :daytime],
+    [Time.parse("19:00"), :night],
+].each do |recommend_time, period|
+  MRecommendedNewsTime.create!(recommend_time: recommend_time, period: period)
+end
