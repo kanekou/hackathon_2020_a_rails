08820f48ca6979068da878a53881630b5e56ab20
d-sawada <d_sawada@ga-tech.co.jp>
2020-08-30 11:16:15 +0900

    Articleにwritten_at追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_30_011136) do
+ActiveRecord::Schema.define(version: 2020_08_30_020917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_08_30_011136) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+    t.datetime "written_at", null: false
     t.index ["topic_id"], name: "index_articles_on_topic_id"
   end
 
