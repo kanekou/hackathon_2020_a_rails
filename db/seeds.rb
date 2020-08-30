355aca9ac163b67ef5e16ec7720186e9781e505e
d-sawada <d_sawada@ga-tech.co.jp>
2020-08-30 10:24:03 +0900

    seedにTopicを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,21 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 [
-    [Time.parse("7:00"), :morning],
-    [Time.parse("12:00"), :daytime],
-    [Time.parse("19:00"), :night]
+  [Time.parse("7:00"), :morning],
+  [Time.parse("12:00"), :daytime],
+  [Time.parse("19:00"), :night]
 ].each do |recommend_time, period|
   MRecommendedNewsTime.create!(recommend_time: recommend_time, period: period)
+end
+
+[
+  { name: "コロナ", code: "COVID-19", default_delivery_time: Time.parse("08:00") },
+  { name: "熱中症", code: "heartstroke", default_delivery_time: Time.parse("08:00") },
+  { name: "健康/運動", code: "exercise", default_delivery_time: Time.parse("08:00") },
+  { name: "ライフハック", code: "lifehack", default_delivery_time: Time.parse("08:00") },
+  { name: "メンタルヘルス", code: "mental", default_delivery_time: Time.parse("08:00") },
+  { name: "食事", code: "meal", default_delivery_time: Time.parse("08:00") },
+  { name: "睡眠", code: "sleep", default_delivery_time: Time.parse("08:00") },
+].each do |params|
+  Topic.create(params)
 end
