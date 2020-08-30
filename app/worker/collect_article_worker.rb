ffd9d14aaec8985576c28b57c46dea15ae3a516c
d-sawada <d_sawada@ga-tech.co.jp>
2020-08-30 10:49:19 +0900

    非同期で記事を収集するworker作成

---
+++ app/worker/collect_article_worker.rb
@@ -0,0 +1,43 @@
+require "net/http"
+require "uri"
+require "json"
+
+class CollectArticleWorker
+  include Sidekiq::Worker
+
+  def perform(*_args)
+    url = ENV["FLASK_URL"] + "/get_news"
+    articles = fetch_articles
+
+    count = 0
+    articles.each do |params|
+      next if Article.find_by(url: params["url"])
+
+      params["topic"] = Topic.find_by(code: params["topic"])
+      Article.create(params)
+      count += 1
+      puts "create new article: #{params["url"]}"
+    end
+
+    if count > 0
+      puts "create #{count} articles"
+    else
+      puts "no articles are created"
+    end
+  end
+
+  private
+
+  def fetch_articles
+    print "fetch articles from flask server...... "
+    res = Net::HTTP.get(URI.parse(url))
+    articles = JSON.parse(res)["articles"]
+    puts "success"
+    articles
+    JSON.parse(res)["articles"]
+  rescue => e
+    puts "failed"
+    puts e
+    []
+  end
+end
